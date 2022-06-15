56bdd7d4b0c393db7b208b8f1265427cba1bdb69
Shiro Kawai <shiro@acm.org>
2022-06-15 01:56:23 -1000

    Add input variable configure-options

--- download-and-install-gauche.sh
+++ download-and-install-gauche.sh
@@ -4,6 +4,7 @@ if [ -z "$version" ]; then
 fi
 
 do_test=$2
+configure_opts=$3
 
 curl -f -o get-gauche.sh https://raw.githubusercontent.com/shirok/get-gauche/master/get-gauche.sh
 chmod +x get-gauche.sh
@@ -16,7 +17,7 @@ case `uname` in
 esac
 
 if [ "$do_test" = 'true' ]; then
-    ./get-gauche.sh --sudo --auto --prefix=$prefix --version=$version
+    ./get-gauche.sh --sudo --auto --prefix=$prefix --version=$version $configure_opts
 else
-    ./get-gauche.sh --sudo --auto --prefix=$prefix --version=$version --skip-tests
+    ./get-gauche.sh --sudo --auto --prefix=$prefix --version=$version --skip-tests $configure_opts
 fi
