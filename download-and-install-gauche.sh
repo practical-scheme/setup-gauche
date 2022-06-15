39864e4f9a6e2d5554c04c921a2c9712157f1616
Shiro Kawai <shiro@acm.org>
2022-06-15 02:15:26 -1000

    Better get-gauche.sh arguments building

--- download-and-install-gauche.sh
+++ download-and-install-gauche.sh
@@ -1,13 +1,10 @@
+opts='--sudo --auto'
+
 version=$1
 if [ -z "$version" ]; then
     version=latest
 fi
-
-do_test=$2
-configure_opts=$3
-
-curl -f -o get-gauche.sh https://raw.githubusercontent.com/shirok/get-gauche/master/get-gauche.sh
-chmod +x get-gauche.sh
+opts="$opts --version=$version"
 
 case `uname` in
     Darwin)
@@ -15,12 +12,20 @@ case `uname` in
     *)
         prefix=/usr;;
 esac
+opts="$opts --prefix=$prefix"
 
+do_test=$2
 if [ "$do_test" = 'true' ]; then
-    cmdline="./get-gauche.sh --sudo --auto --prefix=$prefix --version=$version $configure_opts"
-else
-    cmdline="./get-gauche.sh --sudo --auto --prefix=$prefix --version=$version --skip-tests $configure_opts"
+    opts="$opts --skip-tests"
 fi
 
-echo "Running $cmdline"
-$cmdline
+configure_opts=$3
+if [ ! -z "$configure_opts" ]; then
+    opts="$opts --configure-args="'"'"$configure_opts"'"'
+fi
+
+curl -f -o get-gauche.sh https://raw.githubusercontent.com/shirok/get-gauche/master/get-gauche.sh
+chmod +x get-gauche.sh
+
+echo "Running get-gauche.sh $opts"
+./get-gauche.sh $opts
