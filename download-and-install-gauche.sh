9e0a7c0899fe6020cab9fa1904d79f0db09c73ac
Shiro Kawai <shiro@acm.org>
2022-06-15 08:49:58 -1000

    Change the way to pass options to get-gauche

--- download-and-install-gauche.sh
+++ download-and-install-gauche.sh
@@ -15,17 +15,17 @@ esac
 opts="$opts --prefix=$prefix"
 
 do_test=$2
-if [ "$do_test" = 'true' ]; then
+if [ "$do_test" != 'true' ]; then
     opts="$opts --skip-tests"
 fi
 
 configure_opts=$3
 if [ ! -z "$configure_opts" ]; then
-    opts="$opts --configure-args="'"'"$configure_opts"'"'
+    opts="$opts --configure-args='$configure_opts'"
 fi
 
 curl -f -o get-gauche.sh https://raw.githubusercontent.com/shirok/get-gauche/master/get-gauche.sh
 chmod +x get-gauche.sh
 
 echo "Running get-gauche.sh $opts"
-./get-gauche.sh $opts
+echo ./get-gauche.sh $opts | /bin/bash
